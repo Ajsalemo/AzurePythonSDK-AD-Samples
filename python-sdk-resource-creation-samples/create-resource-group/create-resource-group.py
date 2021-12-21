106378793995b64e333686ccf6e7f49e5e90f26b
Anthony Salemo <ajssalemo@gmail.com>
2021-12-20 19:58:56 -0500

    added a resource group creation script

---
+++ python-sdk-resource-creation-samples/create-resource-group/create-resource-group.py
@@ -0,0 +1,33 @@
+from azure.mgmt.resource import ResourceManagementClient
+from azure.identity import DefaultAzureCredential
+import os
+
+# Acquire a credential object using CLI-based authentication.
+# Reference - https://docs.microsoft.com/en-us/azure/developer/python/azure-sdk-authenticate-hosted-applications#how-to-use-defaultazurecredential-when-accessing-resources
+credential = DefaultAzureCredential()
+
+# Retrieve subscription ID from environment variable.
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+
+resource_client = ResourceManagementClient(credential, subscription_id)
+
+# Provision the resource group.
+rg_result = resource_client.resource_groups.create_or_update(
+    "PythonAzureExample-rg",
+    {
+        "location": "eastus"
+    }
+)
+
+print(f"Provisioned resource group {rg_result.name} in the {rg_result.location} region")
+
+rg_result = resource_client.resource_groups.create_or_update(
+    "PythonAzureExample-rg",
+    {
+        "location": "eastus",
+        "tags": { "environment":"test", "department":"tech" }
+    }
+)
+
+print(f"Updated resource group {rg_result.name} with tags")
+
