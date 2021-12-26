37b3380f3c219454b4233eaf762643f146d697f8
Anthony Salemo <ajssalemo@gmail.com>
2021-12-26 11:45:29 -0500

    validated that the user assigned identity example is now working

---
+++ python-sdk-msi-samples/python-sdk-msi-user-assigned/README.md
@@ -0,0 +1,4 @@
+# python-sdk-msi-user-assigned
+
+https://docs.microsoft.com/en-us/azure/role-based-access-control/role-assignments-portal-managed-identity#user-assigned-managed-identity
+https://github.com/Azure-Samples/compute-python-msi-vm#role-assignement-to-the-msi-credentials
--- python-sdk-msi-samples/python-sdk-msi-user-assigned/msi-user-assigned.py
+++ python-sdk-msi-samples/python-sdk-msi-user-assigned/msi-user-assigned.py
@@ -6,6 +6,7 @@ from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
 CLIENT_ID = os.getenv('CLIENT_ID')
 
 def user_assigned_msi():
+    # You can create a User Assigned Identity and assign it with the following: https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/qs-configure-cli-windows-vm#:~:text=Assign%20User%20Assigned%20Identity%20during%20the%20Creation%20of,VM%20using%20az%20vm%20create.%20...%20See%20More.
     # Create User Assigned MSI Authentication instance
     credentials = ManagedIdentityCredential(
         client_id=CLIENT_ID
@@ -14,9 +15,7 @@ def user_assigned_msi():
     subscription_client = SubscriptionClient(credentials)
     subscription = next(subscription_client.subscriptions.list())
     subscription_id = subscription.subscription_id
-
     resource_client = ResourceManagementClient(credentials, subscription_id)
-
     for resource_group in resource_client.resource_groups.list():
         print(resource_group.name)
 
